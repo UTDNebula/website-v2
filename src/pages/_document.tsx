d572771a3d543572f5d0ea32735431adc035ddca
Jason Antwi-Appah <hey@jasonaa.me>
2023-08-04 20:17:49 -0500

    Add Kallisto and Inter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="font-inter text-haiti">
         <Main />
         <NextScript />
       </body>
