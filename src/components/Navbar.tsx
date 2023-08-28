fffa76cfe2ad08fdf023923f79f1484687072c72
Jason Antwi-Appah <hey@jasonaa.me>
2023-08-28 00:20:23 -0500

    Rough implementation of mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Disclosure } from '@headlessui/react'
 
 type Item = {
     name: string
@@ -76,43 +77,62 @@ const items: TopLevelItem[] = [
     },
 ]
 const Navbar = () => {
+    // TODO: when breakpoint switches from desktop -> mobile or vice versa, close the menu
     return (
-        <nav className={clsx('flex justify-between p-10 items-center w-full h-32 text-white')}>
-            <Link className='justify-self-start' href='/'>
-            <Image src={'/icon-white.svg'} alt={'logo'} width={90} height={70} priority />
-            </Link>
-            <ul className='flex justify-around w-full h-min'>
-                {items.map((item, outerIndex) => (
-                    <li key={`menu-${outerIndex}`} className='group'>
-                        <Link href={item.link} >
-                            {item.name}
-                        {/* TODO: chevron up icon. note rotate doesn't work */}
-                        {item.children && (<span className='group-hover:rotate-180'>^</span>)}
-                        </Link>
-                        {item.children && (
-                            // TODO: this bg is a gradient in figma
-                            <ul className="absolute w-screen bg-black/40 backdrop-blur-md left-0 py-16 px-20 justify-items-center flex-wrap group-hover:flex hidden">
-                                {item.children.map((child, innerIndex) => (
-                                    // TODO link
-                                    <li key={`menu-${outerIndex}-${innerIndex}`} className='hover:transition-none transition-all w-96 border border-white hover:border-opacity-100 border-opacity-0 rounded-3xl p-8 m-8 flex flex-col gap-1'>
-                                        <h2 className="font-bold text-2xl">
-                                            {child.name}
-                                            {/* TODO: arrow right icon */}
-                                        </h2>
-                                        <p>
-                                            {child.description}
-                                        </p>
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </li>
-                ))}
-            </ul>
-            <button className='justify-self-end'>
-                Get Involved
-            </button>
-        </nav>
+        <Disclosure as="div" className="flex justify-between">
+            {({ open, close }) => (
+                <>
+                    <Link className='flex items-center' href='/'>
+                    <Image src={'/icon-white.svg'} alt={'logo'} width={90} height={70} priority />
+                    </Link>
+                    <Disclosure.Button className="md:hidden">
+                        {/* TODO: icon */}
+                        burger
+                    </Disclosure.Button>
+                    <Disclosure.Panel static as="nav" className={clsx(open ? 'flex flex-col absolute top-0 left-0 bg-black/40 backdrop-blur-md' : 'hidden','md:flex justify-between py-10 items-center w-full text-white')}>
+                        <button className='md:hidden justify-self-start' onClick={()=>close()}>
+                            X
+                            {/* TODO: real icon lol */}
+                            </button>
+                        <ul className='flex justify-around w-full h-min flex-col md:flex-row'>
+                            {items.map((item, outerIndex) => (
+                                <li key={`menu-${outerIndex}`} className='group'>
+                                    <Link href={item.link} >
+                                        {item.name}
+                                    {/* TODO: chevron up icon. note rotate doesn't work */}
+                                    {item.children && (<span className='group-hover:rotate-180'>^</span>)}
+                                    </Link>
+                                    {/* TODO: will need to render another disclosure here */}
+                                    {item.children && (
+                                        // TODO: this bg is a gradient in figma
+                                        <ul className="absolute w-screen bg-black/40 backdrop-blur-md left-0 py-16 px-20 justify-items-center flex-wrap group-hover:flex hidden">
+                                            {item.children.map((child, innerIndex) => (
+                                                // TODO link
+                                                <li key={`menu-${outerIndex}-${innerIndex}`} className='hover:transition-none transition-all w-96 border border-white hover:border-opacity-100 border-opacity-0 rounded-3xl p-8 m-8 flex flex-col gap-1'>
+                                                    <h2 className="font-bold text-2xl">
+                                                        {child.name}
+                                                        {/* TODO: arrow right icon */}
+                                                    </h2>
+                                                    <p>
+                                                        {child.description}
+                                                    </p>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                        <button className='justify-self-end'>
+                            {/* TODO: style this button */}
+                            Get Involved
+                        </button>
+                    </Disclosure.Panel>
+                    
+                </>
+            )}
+        </Disclosure>
+        
     )
 }
 
