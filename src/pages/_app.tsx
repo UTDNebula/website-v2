b87040c0b4b711894360cc5335d7e535d3a17510
Tyler Hill <gotyhill@gmail.com>
2023-11-09 13:48:20 -0600

    Run prettier again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,73 +1,76 @@
 import localFont from 'next/font/local';
-import { Inter } from 'next/font/google'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 const kallisto = localFont({
   src: [
     {
       path: '../fonts/Kallisto/Kallisto Thin.otf',
       weight: '100',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../fonts/Kallisto/Kallisto Thin Italic.otf',
       weight: '100',
-      style: 'italic'
+      style: 'italic',
     },
     {
       path: '../fonts/Kallisto/Kallisto Light.otf',
       weight: '300',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../fonts/Kallisto/Kallisto Light Italic.otf',
       weight: '300',
-      style: 'italic'
+      style: 'italic',
     },
     {
       path: '../fonts/Kallisto/Kallisto Medium.otf',
       weight: '500',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../fonts/Kallisto/Kallisto Medium Italic.otf',
       weight: '500',
-      style: 'italic'
+      style: 'italic',
     },
     {
       path: '../fonts/Kallisto/Kallisto Bold.otf',
       weight: '700',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../fonts/Kallisto/Kallisto Bold Italic.otf',
       weight: '700',
-      style: 'italic'
+      style: 'italic',
     },
     {
       path: '../fonts/Kallisto/Kallisto Heavy.otf',
       weight: '900',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../fonts/Kallisto/Kallisto Heavy Italic.otf',
       weight: '900',
-      style: 'italic'
-    }
+      style: 'italic',
+    },
   ],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <style jsx global>{`
+  return (
+    <>
+      <style jsx global>
+        {`
           :root {
             --font-kallisto: ${kallisto.style.fontFamily};
             --font-inter: ${inter.style.fontFamily};
           }
         `}
       </style>
-    <Component {...pageProps} />
-  </>
+      <Component {...pageProps} />
+    </>
+  );
 }
