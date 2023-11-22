f2602773325041975d3ef1c78c0ab28fcedb6b19
Jason Antwi-Appah <hey@jasonaa.me>
2023-11-21 23:53:55 -0600

    Format

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -27,7 +27,9 @@ const CTA = () => {
         }}
       >
         <Image src={'/icon-white.svg'} alt={'logo'} width={100} height={0} />
-        <h3 className="text-5xl tracking-tight leading-[3.25rem]">Ready to explore the stars with us?</h3>
+        <h3 className="text-5xl tracking-tight leading-[3.25rem]">
+          Ready to explore the stars with us?
+        </h3>
         <p className="tracking-tight text-xl">
           Join Nebula Labs today and be a part of our mission to innovate and create tools that make
           a difference in the world.
