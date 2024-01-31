a6f89880b5496f87aa0ad484599f2a4d368089f0
Tyler Hill <gotyhill@gmail.com>
2024-01-30 22:42:42 -0600

    Proper 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,14 @@
-# Not Found
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
-The requested page was not found on this site.
+const _404 = () => (
+  <div className="bg-white">
+    <Header text="Not Found" />
+    <h2 className="px-8 lg:px-16 xl:px-32 pb-12 text-5xl font-bold text-center">
+      The requested page was not found on this site.
+    </h2>
+    <Footer royalBg={false} />
+  </div>
+);
+
+export default _404;
