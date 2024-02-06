0de4ff88b41a46b047a28f381a328953e03653bf
dlaunikitis <david.launikitis@gmail.com>
2024-02-06 16:39:01 -0600

    Create dictionary.tsx
    Created and populated the dictionary file that will include all the people within leadership

---
+++ src/data/dictionary.tsx
@@ -0,0 +1,123 @@
+interface Person {
+  name: string;
+  linkedIn: string;
+  imageLink: string | undefined;
+}
+
+let myMap = new Map<string, Person>([
+    ["cnl210000", {
+        name: "Caleb Lim",
+        linkedIn: "caleeb-lim",
+        imageLink: undefined
+    }],
+    ["dml190001", {
+        name: "",
+        linkedIn: "dlaunikitis",
+        imageLink: undefined
+    }],
+    ["axr200071", {
+        name: "David Launikitis",
+        linkedIn: "amrit-rathie",
+        imageLink: undefined
+    }],
+    ["jrs190008", {
+        name: "Jake Spann",
+        linkedIn: "jake-spann-35210615a",
+        imageLink: undefined
+    }],
+    ["sxd200087", {
+        name: "Shaurya Dwivedi",
+        linkedIn: "shaurya-dwivedi",
+        imageLink: undefined
+    }],
+    ["jxa220013", {
+        name: "Jason Antwi-Appah",
+        linkedIn: "jasonaa",
+        imageLink: undefined
+    }],
+    ["sxl220024", {
+        name: "Sharon Lnu",
+        linkedIn: "sharon-lnu-6949801a3",
+        imageLink: undefined
+    }],
+    ["flh220002", {
+        name: "Frances Hill",
+        linkedIn: "flhill",
+        imageLink: undefined
+    }],
+    ["npn190002", {
+        name: "Hilary Nguyen",
+        linkedIn: "hilary-nguyen",
+        imageLink: undefined
+    }],
+    ["wgs190000", {
+        name: "William Skaggs",
+        linkedIn: "william-skaggs-4b86881b7",
+        imageLink: undefined
+    }],
+    ["rdo190000", {
+        name: "Ruben Olano",
+        linkedIn: "rubendolano",
+        imageLink: undefined
+    }],
+    ["jjp210000", {
+        name: "Josh Pahman",
+        linkedIn: "jpahm",
+        imageLink: undefined
+    }],
+    ["rxt220032", {
+        name: "Ragini Tiwari",
+        linkedIn: "msraginitiwari",
+        imageLink: undefined
+    }],
+    ["Ryan.Radloff", {
+        name: "Ryan Radloff",
+        linkedIn: "ryan-radloff",
+        imageLink: undefined
+    }],
+    ["ecb180001", {
+        name: "Eric Boysen",
+        linkedIn: "eric-boysen-a16243192",
+        imageLink: undefined
+    }],
+    ["cbm200003", {
+        name: "Charlie Mahana",
+        linkedIn: "charliemahana",
+        imageLink: undefined
+    }],
+    ["rsj180004", {
+        name: "Rajmeet Juneja",
+        linkedIn: "rajmeetjuneja",
+        imageLink: undefined
+    }],
+    ["Nate.Williams", {
+        name: "Nathan Williams",
+        linkedIn: "nathan-williams-357a5b61",
+        imageLink: undefined
+    }],
+    ["Shreyas.Subramanian", {
+        name: "Shreyas Subramanian",
+        linkedIn: "shreyas-subramanian",
+        imageLink: undefined
+    }],
+    ["Harsha.Ketaraju", {
+        name: "Harsha Ketaraju",
+        linkedIn: "harsha-ketaraju",
+        imageLink: undefined
+    }],
+    ["amb150230", {
+        name: "Adam Brunn",
+        linkedIn: "adammcadamson",
+        imageLink: undefined
+    }],
+    ["wec190000", {
+        name: "Willie Chalmers III",
+        linkedIn: "willie-chalmers-iii",
+        imageLink: undefined
+    }],
+    ["axs200279", {
+        name: "Adam Szumski",
+        linkedIn: "aszumski",
+        imageLink: undefined
+    }]
+]);
