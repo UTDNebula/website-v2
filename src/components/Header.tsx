0ae7a82409415f283c9e83a9e19c242140f6d57d
Tyler Hill <gotyhill@gmail.com>
2024-04-27 01:20:11 -0500

    Revert Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,12 @@ import Navbar from '@/components/Navbar';
 
 interface Props {
   text: string;
-  image?: JSX.Element,
 }
 
 const Header = (props: Props) => {
   return (
     <div className="bg-gradient-to-b from-royal to-white to-75%">
       <Navbar />
-      <div className=" flex justify-center items-center">
-        {props.image}
-      </div>
       <div className="px-8 h-[20vh] flex justify-center items-center">
         <h1 className="text-6xl font-bold text-royal text-center">{props.text}</h1>
       </div>
