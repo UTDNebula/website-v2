02cb9dd623e35589c44587a4f678ed209980dc83
Tyler Hill <gotyhill@gmail.com>
2024-04-27 00:28:30 -0500

    Filled in mission

--- src/pages/about/mission.tsx
+++ src/pages/about/mission.tsx
@@ -37,14 +37,30 @@ const Mission = () => (
       <meta property="og:url" content="https://www.utdnebula.com/resources/roles" />
     </Head>
     <Header text="Mission" />
-    <div className="px-8 lg:px-16 xl:px-32 py-24 flex flex-wrap justify-center gap-8">
-      <p className="text-3xl pb-4">
-        Whereas students often lack tools that assist in their academic and career success; whereas
-        students benefit from applying their knowledge outside of the classroom; and whereas there
-        should be a place to learn and have fun while building tools for the community, we establish
-        the Nebula Labs organization to build and maintain tools that help students, to foster an
-        interdisciplinary and welcoming community, and to teach the community what we learn.
+    <div className="px-8 lg:px-16 xl:px-32 py-24 flex flex-col justify-center gap-8">
+      <p className="text-3xl pb-4 leading-normal">
+        The heart of Nebula Labs&apos; mission is the development of tools to assist the student
+        body of UT Dallas (or other interested student bodies). The organization’s founding members,
+        dissatisfied with the software tools offered by the University and cognizant of the
+        University’s reputation for technologically talented individuals, set out to make a suite of
+        tools for use by the student body to improve student life in all aspects.
       </p>
+      <p className="text-3xl pb-4 leading-normal">
+        In this mission, the organization commits itself to building tools that assist students in
+        their academic, social, and personal lives here at UTD so as to enable them to make the most
+        of the experiences and opportunities therein.
+      </p>
+      <p className="text-3xl pb-4 leading-normal">
+        In that vein, we believe that working, learning, and mentoring as a part of Nebula Labs is
+        one such opportunity that we can offer to improve student life directly. This means that the
+        organization, while its primary focus is development, is also focused on the personal
+        development of its membership and strives to ensure its work can be used to better the
+        personal and professional lives of its members. In the words of our first President,
+      </p>
+      <p className="text-3xl leading-normal text-center italic">
+        &lsquo;&lsquo;We build products and people.&rsquo;&rsquo;
+      </p>
+      <p className="text-3xl pb-4 leading-normal text-center italic">-Ryan Radloff</p>
     </div>
     <div className="px-8 lg:px-16 xl:px-32 py-24 flex flex-wrap justify-center gap-8">
       <h2 className="text-5xl font-bold pb-4 text-center">Values</h2>
