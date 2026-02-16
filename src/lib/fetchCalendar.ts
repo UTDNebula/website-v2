abecd349a7684ddac712bc238e68e32574eaea20
Tyler Hill <gotyhill@gmail.com>
2026-02-15 18:06:49 -0600

    Backup link

--- src/lib/fetchCalendar.ts
+++ src/lib/fetchCalendar.ts
@@ -1,7 +1,5 @@
+import { CALENDAR_ID } from '@/data/calendarId';
 import { auth, JWT } from 'google-auth-library';
-
-const CALENDAR_ID =
-  'c_81b7102868d4acac8b7db3a18de6440d45740e4754be4f8a28a5c3915b0d1e71@group.calendar.google.com';
 
 type Data = {
   message: string;
